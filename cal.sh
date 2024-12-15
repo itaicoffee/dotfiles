41cec0dcd1246900588d8e37d8d6bc3d3b687385
itai <idh1729@gmail.com>
2024-12-15 12:36:52 -0500

    Move to bin folder

--- cal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CHROME_PROFILE=$CHROME_PROFILE_WORK || 'Default'
-url 'https://calendar.google.com' $CHROME_PROFILE
-close_iterm
--- glean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-url $(url_query 'https://app.glean.com/search?q={query}' $@) 'Default'
-close_iterm
